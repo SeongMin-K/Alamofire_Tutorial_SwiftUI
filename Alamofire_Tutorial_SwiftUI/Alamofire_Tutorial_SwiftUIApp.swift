8d9c5c41276c8aa71725484aed8584b3c916843e
SeongMin-K <eo1tks3@naver.com>
2021-12-18 17:25:58 +0900

    Initial Commit

---
+++ Alamofire_Tutorial_SwiftUI/Alamofire_Tutorial_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alamofire_Tutorial_SwiftUIApp.swift
+//  Alamofire_Tutorial_SwiftUI
+//
+//  Created by SeongMinK on 2021/12/18.
+//
+
+import SwiftUI
+
+@main
+struct Alamofire_Tutorial_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alamofire_Tutorial_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alamofire_Tutorial_SwiftUI
+//
+//  Created by SeongMinK on 2021/12/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
